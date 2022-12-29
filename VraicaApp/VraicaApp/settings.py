62805f22158003c7089aeae4547f72682ebeafec
vraica-dev <rvg.vali@gmail.com>
2022-12-29 21:11:07 +0200

    small chg on settings

--- VraicaApp/VraicaApp/settings.py
+++ VraicaApp/VraicaApp/settings.py
@@ -134,7 +134,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static/"),
 ]
